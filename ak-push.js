7ef1a87afae2e0fea02fe5e7938932acaac14a3b
MaksimLavrenyuk <42980967+MaksimLavrenyuk@users.noreply.github.com>
2019-12-12 15:41:25 +0300

    Add files via upload

--- ak-push.js
+++ ak-push.js
@@ -1,7 +1,7 @@
 // Usage:
-// var akPush = new AKPush()
-// akPush.sendCookies();
+//
 // try {
+//     var akPush = new AKPush()
 //     akPush.initSubscription() // Show push subscribe browser popup
 //     // ...
 // } catch (e) {
@@ -121,7 +121,6 @@
             serverCookiePath: "/pixel?" + ["_push_pix", "/set_cookie_only"].join("="),
             swPath: "/service-worker.js",
             isTest: false,
-            cookieID: "",
             debug: false,
             browsers: {},
             firebase: {
@@ -184,7 +183,7 @@
             })
         }
 
-        this.sendCookies = function() {
+        this.sendCookies = function(callback) {
             fetch(this.config.serverURL + this.config.serverCookiePath, {
                 method: 'post',
                 credentials: 'include',
@@ -193,7 +192,7 @@
                 return response.json();
             }).then(function(data) {
                 if ('cookie_id' in data) {
-                    that.config.cookieID = data['cookie_id']
+                    callback(data['cookie_id']);
                 } else {
                     console.error('Invalid response for set cookie:', data);
                 }
@@ -305,7 +304,6 @@
         this.subscribeBasic = function(match, update, customData) {
             navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
                 serviceWorkerRegistration.pushManager.subscribe({ userVisibleOnly: true }).then(function(subscription) {
-                    console.log(subscription.applicationServerKey)
                     that.sendSubscription("save", that.config.browser, subscription, match, update, customData);
                 }).catch(function(err) {
                     if (Notification.permission === 'denied') {
@@ -411,9 +409,11 @@
                     break;
                 case "Safari":
                     this.debug("Initialise subscription for: " + this.config.browser + " with Safari")
-                    let permissionData = window.safari.pushNotification.permission(that.config.browsers.Safari.websitePushID);
-                    that.debug("Permission data: ", permissionData)
-                    that.initialiseSafariPush(permissionData, match, update,  that.config.cookieID, customData);
+                    this.sendCookies(function(cookieId) { //TODO: m.b. this works slowly
+                        let permissionData = window.safari.pushNotification.permission(that.config.browsers.Safari.websitePushID);
+                        that.debug("Permission data: ", permissionData)
+                        that.initialiseSafariPush(permissionData, match, update, cookieId, customData);
+                    });
                     break;
                 default:
                     console.error("Browser is not supported: ", this.config.browser)
