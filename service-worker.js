57a46a323fdcaaa38deadc95f77515cb1d2cb97c
Vital112 <54412863+Vital112@users.noreply.github.com>
2020-03-03 16:14:12 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://pxl.vitaly-rizaev.dev.altkraft.com/service-worker.js?id=MXwxMjA.');
+importScripts('https://pxl.vitaly-rizaev.dev.altkraft.com/service-worker.js?id=MXwxMjE.');
